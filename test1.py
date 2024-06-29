695a84f32571d04f0f91180c7bec0e50e46477f2
Andrew G <andrew2000g@gmail.com>
2024-06-29 14:33:46 -0700

    epic SAE code

---
+++ test1.py
@@ -0,0 +1,92 @@
+from functools import partial
+from transformer_lens import utils
+
+import torch as t
+from datasets import load_dataset
+from sae_lens import SAE
+from transformer_lens import HookedTransformer
+from transformer_lens.utils import tokenize_and_concatenate
+
+if t.backends.mps.is_available():
+    device = "mps"
+else:
+    device = "cuda" if t.cuda.is_available() else "cpu"
+
+# Load the model
+model = HookedTransformer.from_pretrained("gpt2-small", device=device)
+
+# Load the sparse autoencoder
+sae, cfg_dict, sparsity = SAE.from_pretrained(
+    release="gpt2-small-res-jb",
+    sae_id="blocks.8.hook_resid_pre",
+    device=device
+)
+
+# Load the dataset
+dataset = load_dataset(
+    path="NeelNanda/pile-10k",
+    split="train",
+    streaming=False,
+)
+
+token_dataset = tokenize_and_concatenate(
+    dataset=dataset,  # type: ignore
+    tokenizer=model.tokenizer,  # type: ignore
+    streaming=True,
+    max_length=sae.cfg.context_size,
+    add_bos_token=sae.cfg.prepend_bos,
+)
+
+# Test the IOI thing
+
+example_prompt = "When John and Mary went to the shops, John gave the bag to"
+example_answer = " Mary"
+utils.test_prompt(example_prompt, example_answer, model, prepend_bos=True)
+
+logits, cache = model.run_with_cache(example_prompt, prepend_bos=True)
+tokens = model.to_tokens(example_prompt)
+sae_out = sae(cache[sae.cfg.hook_name])
+
+
+def reconstr_hook(activations, hook, sae_out):
+    return sae_out
+
+
+def zero_abl_hook(mlp_out, hook):
+    return t.zeros_like(mlp_out)
+
+
+hook_name = sae.cfg.hook_name
+
+print("Orig", model(tokens, return_type="loss").item())
+print(
+    "reconstr",
+    model.run_with_hooks(
+        tokens,
+        fwd_hooks=[
+            (
+                hook_name,
+                partial(reconstr_hook, sae_out=sae_out),
+            )
+        ],
+        return_type="loss",
+    ).item(),
+)
+print(
+    "Zero",
+    model.run_with_hooks(
+        tokens,
+        return_type="loss",
+        fwd_hooks=[(hook_name, zero_abl_hook)],
+    ).item(),
+)
+
+with model.hooks(
+        fwd_hooks=[
+            (
+                    hook_name,
+                    partial(reconstr_hook, sae_out=sae_out),
+            )
+        ]
+):
+    utils.test_prompt(example_prompt, example_answer, model, prepend_bos=True)
