98ff5928f7a4754dda3460a3c8c0492cdd12f069
Varun Swaminathan <85042815+flerovious@users.noreply.github.com>
2024-06-29 18:24:43 -0700

    Add tts server

---
+++ tts_server.py
@@ -0,0 +1,53 @@
+import time
+
+import torch
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+from TTS.api import TTS
+
+if torch.backends.mps.is_available():
+    device = "mps"
+else:
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# List available 🐸TTS models
+print(TTS().list_models())
+
+# Init TTS
+tts = TTS("tts_models/en/ljspeech/tacotron2-DDC").to(device)
+
+
+app = FastAPI()
+
+origins = ["*"]  # fix later
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.post("/")
+async def respond():
+    time.sleep(1)
+    return "I am the Golden Gate - Leo"
+
+
+@app.post("/tts")
+async def tts():
+    file_path = "output.wav"
+
+    # Text to speech to a file
+    tts.tts_to_file(text="This is a new file", file_path=file_path)
+
+    # Return the wav file as a response
+    return FileResponse(path=file_path, media_type="audio/wav", filename=file_path)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
