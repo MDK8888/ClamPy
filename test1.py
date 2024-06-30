b1b5c16553410185586ff07b2e124dac63dd528f
Shubham Mohole <143766726+Shub500@users.noreply.github.com>
2024-06-30 14:54:00 -0400

    Hook Parameterization

--- test1.py
+++ test1.py
@@ -30,15 +30,14 @@ dataset = load_dataset(
 )
 
 token_dataset = tokenize_and_concatenate(
-    dataset=dataset,  # type: ignore
-    tokenizer=model.tokenizer,  # type: ignore
+    dataset=dataset,
+    tokenizer=model.tokenizer,
     streaming=True,
     max_length=sae.cfg.context_size,
     add_bos_token=sae.cfg.prepend_bos,
 )
 
 # Test the IOI thing
-
 example_prompt = "When John and Mary went to the shops, John gave the bag to"
 example_answer = " Mary"
 utils.test_prompt(example_prompt, example_answer, model, prepend_bos=True)
@@ -47,21 +46,18 @@ logits, cache = model.run_with_cache(example_prompt, prepend_bos=True)
 tokens = model.to_tokens(example_prompt)
 sae_out = sae(cache[sae.cfg.hook_name])
 
-
-def reconstr_hook(activations, hook, sae_out):
-    # return sae_out
-    steered_activations = activations
-    # zero out the 5th column
-    steered_activations[:, :, 5] = 0
+# Define hooks
+def reconstr_hook(activations, hook, sae_out, column_to_zero=5):
+    steered_activations = activations.clone()
+    steered_activations[:, :, column_to_zero] = 0
     return steered_activations
-
 
 def zero_abl_hook(mlp_out, hook):
     return t.zeros_like(mlp_out)
 
-
 hook_name = sae.cfg.hook_name
 
+# Run with hooks
 print("Orig", model(tokens, return_type="loss").item())
 print(
     "reconstr",
@@ -85,12 +81,24 @@ print(
     ).item(),
 )
 
+# Run with hooks and test prompt
 with model.hooks(
-        fwd_hooks=[
-            (
-                    hook_name,
-                    partial(reconstr_hook, sae_out=sae_out),
-            )
-        ]
+    fwd_hooks=[
+        (
+            hook_name,
+            partial(reconstr_hook, sae_out=sae_out),
+        )
+    ]
 ):
     utils.test_prompt(example_prompt, example_answer, model, prepend_bos=True)
+
+# Example of using the hook with different column to zero
+with model.hooks(
+    fwd_hooks=[
+        (
+            hook_name,
+            partial(reconstr_hook, sae_out=sae_out, column_to_zero=10),
+        )
+    ]
+):
+    print("Loss with column 10 zeroed:", model(tokens, return_type="loss").item())
